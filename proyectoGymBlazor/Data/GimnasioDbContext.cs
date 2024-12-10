1901a6d60d209f9cf361ccdd90e75b121a0d90d6
Randsiwim <randsiwin13@gmail.com>
2024-12-10 09:39:01 -0600

    conexion DB Gym.v2

---
+++ proyectoGymBlazor/Data/GimnasioDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using proyectoGym;
+using System.Collections.Generic;
+
+namespace proyectoGymBlazor.Data
+{
+    public class GimnasioDbContext : DbContext
+    {
+        public GimnasioDbContext(DbContextOptions<GimnasioDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Membresia> Membresias { get; set; }
+        public DbSet<Clase> Clases { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+        public DbSet<Inventario> Inventarios { get; set; }
+        public DbSet<Factura> Facturas { get; set; }
+        public DbSet<ProgresoUsuario> ProgresoUsuarios { get; set; }
+    }
+}
+
